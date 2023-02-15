079198140cc254a13352f12c623caea9d63813fd
Andrew Babbitt <Andrew.Babbitt@outlook.com>
2023-02-15 08:57:56 -0700

    chore: implement primer models

---
+++ src/BlazorDocs.Primer/Models/Config.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the Blazor Docs Contributors under one or more agreements.
+// The Blazor Docs Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BlazorDocs.Models;
+
+namespace BlazorDocs.Primer.Models;
+
+/// <summary>
+/// The config.
+/// </summary>
+public sealed class Config : DocumentationConfig
+{
+    /// <summary>
+    /// The path to the logo image.
+    /// </summary>
+    public string LogoPath { init; get; } = string.Empty;
+}
---
+++ src/BlazorDocs.Primer/Models/Page.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the Blazor Docs Contributors under one or more agreements.
+// The Blazor Docs Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BlazorDocs.Models;
+
+namespace BlazorDocs.Primer.Models;
+
+/// <summary>
+/// A page.
+/// </summary>
+public sealed class Page : DocumentationPage<Page>
+{
+    /// <summary>
+    /// The short title.
+    /// </summary>
+    public string ShortTitle { init; get; } = string.Empty;
+}
